d84c718c859f4c263b538d5d26440522b448b253
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-07-24 16:30:00 +0000

    bump SW cache to v2 for rebrand refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fuelfinder-v1';
+const CACHE='fuelfinder-v2';
 const ASSETS=['./','./index.html','./manifest.json','./stations.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
